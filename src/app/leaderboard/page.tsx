3d8d2499a1a416443d1779b0f1571ed2132b6c50
Hriz <harizof@gmail.com>
2025-07-16 11:34:29 +0700

    Added Leaderboard Page

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -1,7 +1,11 @@
-export default function LeaderboardPage() {
+import Sidebar from '@/components/sidebar/Sidebar'
+import LeaderboardPage from '@/components/Leaderboard/Main'
+
+export default function Home() {
   return (
-    <main>
-      <p>Coming soon</p>
-    </main>
+    <div className="min-h-screen bg-white/90 dark:bg-gray-900 p-2 lg:p-4 flex items-center justify-center">
+      <Sidebar />
+      <LeaderboardPage />
+    </div>
   )
 }
